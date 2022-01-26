868e8ca3481023abf427f760e01bb29f8b9c02e4
krangelie <kraftangelie@gmail.com>
2022-01-26 11:22:20 +0100

    adding some scripts from Junbo

---
+++ main.py
@@ -0,0 +1,13 @@
+import logging
+import warnings
+
+
+
+if __name__ == "__main__":
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    warnings.filterwarnings("ignore")
+
+    pass
+
+
---
+++ utils.py
@@ -0,0 +1,29 @@
+import random
+import mlflow
+import numpy as np
+import torch
+from hydra import initialize, compose
+from omegaconf import DictConfig, ListConfig
+
+
+def set_seed(seed: int):
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+
+
+def log_params_from_omegaconf_dict(params):
+    for param_name, element in params.items():
+        _explore_recursive(param_name, element)
+
+
+def _explore_recursive(parent_name, element):
+    if isinstance(element, DictConfig):
+        for k, v in element.items():
+            if isinstance(v, DictConfig) or isinstance(v, ListConfig):
+                _explore_recursive(f'{parent_name}.{k}', v)
+            else:
+                mlflow.log_param(f'{parent_name}.{k}', v)
+    elif isinstance(element, ListConfig):
+        for i, v in enumerate(element):
+            mlflow.log_param(f'{parent_name}.{i}', v)
