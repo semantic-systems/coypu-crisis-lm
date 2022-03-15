ff091326b84f9aed326a2f51e9c815fc7620e3f6
krangelie <kraftangelie@gmail.com>
2022-03-15 15:38:39 +0100

    small fix

--- src/train_mlm.py
+++ src/train_mlm.py
@@ -89,11 +89,11 @@ def _eval_model(cfg, trainer, training_args, logger):
 
 
 def train(cfg, logger):
+    if cfg.model.name == "bertweet":
+        normalization = True
+    else:
+        normalization = False
     if cfg.architecture == "mlm":
-        if cfg.model.name == "bertweet":
-            normalization = True
-        else:
-            normalization = False
         model, tokenizer = get_model_and_tokenizer(cfg.model.pretrained_model, cfg.architecture,
                                                    cfg.mode.freeze_encoder, normalization=normalization)
         data_collator = get_data_collator(cfg.architecture, tokenizer, cfg.model.mlm_probability,
@@ -101,7 +101,8 @@ def train(cfg, logger):
         compute_metrics = None
     elif cfg.architecture == "seq":
         model, tokenizer = get_model_and_tokenizer(cfg.model.pretrained_model, cfg.architecture,
-                                                   cfg.mode.freeze_encoder, 2 if cfg.task == "informativeness" else 11)
+                                                   cfg.mode.freeze_encoder, normalization=normalization,
+                                                   num_labels=2 if cfg.task == "informativeness" else 11)
         data_collator = None
         compute_metrics = _classifier_metrics
     else:
