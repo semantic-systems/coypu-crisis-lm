0285b485e85bad87531718dcbb30878c3cd9abfe
krangelie <kraftangelie@gmail.com>
2022-08-31 20:58:22 +0200

    train adapter script

---
+++ src/train_adapter.py
@@ -0,0 +1,4 @@
+""" Train an Adapter via AdapterHub, https://adapterhub.ml/.
+https://docs.adapterhub.ml/training.html
+- bottleneck size 48 (compression rate 16 - performance-wise sweet spot
+- Pfeiffer variant (one bottleneck)"""
